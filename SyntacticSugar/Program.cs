146e91a9fe3614de56ab0bef7edb46263b4dd732
Kelsey Creel <kelseycreel@gmail.com>
2020-03-14 10:54:22 -0500

    syntax

--- SyntacticSugar/Program.cs
+++ SyntacticSugar/Program.cs
@@ -17,52 +17,10 @@ namespace SyntacticSugar
             var catPrey = new List<string>();
             catPrey.Add("mouse");
 
+            var cat = new Bug("house cat", "feline", catPredators, catPrey);
 
-            // Convert this readonly property to an expression member
-            public string FormalName
-            {
-                 
-                {
-                    return $"{this.Name} the {this.Species}";
-                }
-            }
-
-            // Class constructor
-            public Bug(string name, string species, List<string> predators, List<string> prey)
-            {
-                this.Name = name;
-                this.Species = species;
-                this.Predators = predators;
-                this.Prey = prey;
-            }
-
-            // Convert this method to an expression member
-            public string PreyList()
-            {
-                var commaDelimitedPrey = string.Join(",", this.Prey);
-                return commaDelimitedPrey;
-            }
-
-            // Convert this method to an expression member
-            public string PredatorList()
-            {
-                var commaDelimitedPredators = string.Join(",", this.Predators);
-                return commaDelimitedPredators;
-            }
-
-            // Convert this to expression method
-            public string Eat(string food)
-            {
-                if (this.Prey.Contains(food))
-                {
-                    return $"{this.Name} ate the {food}.";
-                }
-                else
-                {
-                    return $"{this.Name} is still hungry.";
-                }
-            }
+            Console.WriteLine(cat.Eat("kibble"));
+            Console.ReadKey();
         }
-    }
     }
 }
